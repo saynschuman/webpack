1283fe504172eb0282c59177923bf29c8869a766
saynschuman <saynschuman@gmail.com>
2020-05-04 18:23:37 +0300

    search form render

--- src/features/search/SearchPage.js
+++ src/features/search/SearchPage.js
@@ -1,7 +1,58 @@
 import React from "react";
+import {
+  Container,
+  FormGroup,
+  Input,
+  ListGroup,
+  ListGroupItem,
+  Card,
+  CardHeader,
+  CardBody,
+  Form,
+  Button,
+} from "reactstrap";
+import { FaSearchLocation } from "react-icons/fa";
 
 const SearchPage = () => {
-  return <div>SearchPage</div>;
+  const [city, setCity] = React.useState("");
+  const onChange = (e) => {
+    setCity(e.target.value);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <Container>
+      <Card>
+        <CardHeader>
+          <Form onSubmit={onSubmit}>
+            <FormGroup className="d-flex flex-nowrap justify-content-between">
+              <Input
+                className="w-75"
+                type="search"
+                name="search"
+                value={city}
+                placeholder="Введите город"
+                onChange={onChange}
+              />
+              <Button className="w-25 ml-2">
+                <FaSearchLocation />
+              </Button>
+            </FormGroup>
+          </Form>
+        </CardHeader>
+        <CardBody>
+          <ListGroup>
+            <ListGroupItem>Cras justo odio</ListGroupItem>
+            <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+            <ListGroupItem>Morbi leo risus</ListGroupItem>
+            <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
+            <ListGroupItem>Vestibulum at eros</ListGroupItem>
+          </ListGroup>
+        </CardBody>
+      </Card>
+    </Container>
+  );
 };
 
 export default SearchPage;
